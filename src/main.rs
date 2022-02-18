d56e0d48113b4ea8c878826ccc6cfd1de7d2a4d2
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-02-18 12:24:34 -0500

    importing matrix, fixed some bugs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use image::Image;
 use color::Color;
+use matrix::Matrix;
 mod image;
 mod color;
+mod matrix;
 
 fn main(){
     
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,17 +1,19 @@
-struct Matrix{
-    pub matrixArray: vec!<vec!<i32>>;
+pub struct Matrix{
+    pub matrixArray: Vec<Vec<i32>>,
 }
 
 impl Matrix{
-    fn new(row: usize = 4, col: usize = 4) -> Matrix{
+    fn new(row: Option<usize>, col: Option<usize>) -> Matrix{
+        let row = row.unwrap_or(4);
+        let col = col.unwrap_or(4);
         Matrix{matrixArray: vec![vec![0; row]; col],}
     }
 
     fn multiply_matrixes(&self, m1: Matrix){
-
+        
     }
 
-    fn identity(&self){
+    fn identity(&mut self){
         for i in 0..self.matrixArray.len(){
             for v in 0..self.matrixArray[0].len(){
                 if i == v{

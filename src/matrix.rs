a64150d797e604289439dee114b78d5ce4492a78
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-02-18 17:39:01 -0500

    matrix multiply and printing works

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,16 +1,26 @@
+use std::fmt;
+
 pub struct Matrix{
     pub matrixArray: Vec<Vec<i32>>,
 }
 
 impl Matrix{
-    fn new(row: Option<usize>, col: Option<usize>) -> Matrix{
-        let row = row.unwrap_or(4);
-        let col = col.unwrap_or(4);
+    pub fn new(row: usize, col: usize) -> Matrix{
         Matrix{matrixArray: vec![vec![0; row]; col],}
     }
 
-    fn multiply_matrixes(&self, m1: Matrix){
-        
+    pub fn multiply_matrixes(&mut self, m1: Matrix){
+        let mut matrix_result = Matrix::new(m1.matrixArray.len(), self.matrixArray[0].len());
+        println!("{}", matrix_result);
+        for result_i in 0..matrix_result.matrixArray.len(){
+            for result_v in 0..matrix_result.matrixArray[result_i].len(){
+                for m2_down_num in 0..self.matrixArray.len(){
+                    matrix_result.matrixArray[result_i][result_v] += self.matrixArray[m2_down_num][result_v] * m1.matrixArray[result_i][m2_down_num];
+                }
+            }
+        }
+
+        *self = matrix_result;
     }
 
     fn identity(&mut self){
@@ -23,5 +33,18 @@ impl Matrix{
                 }
             }
         }
+    }
+}
+
+impl fmt::Display for Matrix{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        let mut result: String = "".to_owned();
+        for i in 0..self.matrixArray.len(){
+            for v in 0..self.matrixArray[i].len(){
+                result.push_str(&(format!("{} ",self.matrixArray[i][v]).to_string()));
+            }
+            result.push_str("\n");
+        }
+        write!(f, "{}", result)
     }
 }
